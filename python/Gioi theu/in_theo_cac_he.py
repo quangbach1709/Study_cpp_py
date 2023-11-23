39ead5cf4655e255dede60b3ecad05710b021259
QuangBach <bachh1124@gmail.com>
2023-11-23 22:41:18 +0700

    upload

--- python/Gioi theu/in_theo_cac_he.py
+++
@@ -1,4 +0,0 @@
-n=int(input("Nhap so nguyen N: "))
-print("N o dang co so 16 la:",hex(n))
-print("N o dang co so 8 la: ",oct(n))
-print("N o dang co so 2 la: ",bin(n))
--- python/Gioi theu/tinh_can_bac_bcua_a.py
+++
@@ -1,3 +0,0 @@
-a=int(input("Nhap so nguyen A = "))
-b=int(input("Nhap so nguyen B = "))
-print("Ket qua la: ",pow(a,1/b))
